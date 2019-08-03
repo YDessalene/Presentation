8952a6cc3457a77ec57946ffa4ae6c267aa8be08
YDessalene <37000615+YDessalene@users.noreply.github.com>
2019-08-03 11:55:28 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+window.onload = function() {
+
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    
+    function preload() {
+
+    }
+        
+    function create() {
+        
+    }
+    
+    function update() {
+        
+    }
+};
